e73e9f04e7bd8969ffa887511668d743e5e65aae
Fenja <fenja42@hotmail.de>
2022-01-25 21:37:50 +0100

    :sparkles: backbutton for navigation

--- lib/widgets/screen_box.dart
+++ lib/widgets/screen_box.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// this file will collect styles for use across the app
@@ -6,30 +5,35 @@ import 'package:flutter/material.dart';
 class ScreenBox extends StatelessWidget {
   const ScreenBox({
     Key? key,
-    required this.child
+    required this.child,
   }) : super(key: key);
 
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          repeat: ImageRepeat.repeat,
-          image: AssetImage('images/bg_pattern.png'),
-          scale: 15.0,
-          opacity: 0.2,
-          // colorFilter: ColorFilter.mode(Colors.white, BlendMode.hue)
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              repeat: ImageRepeat.repeat,
+              image: AssetImage('images/bg_pattern.png'),
+              scale: 15.0,
+              opacity: 0.2,
+              // colorFilter: ColorFilter.mode(Colors.white, BlendMode.hue)
+            ),
+          ),
+          alignment: FractionalOffset.center,
+          padding: const EdgeInsets.only(
+              left: 20.0, right: 20.0, bottom: 5.0, top: 5.0),
+          child: SizedBox(
+              width: 600,
+              child: child
+          ),
         ),
-      ),
-      alignment: FractionalOffset.center,
-      padding: const EdgeInsets.only(
-          left: 20.0, right: 20.0, bottom: 5.0, top: 5.0),
-      child: SizedBox(
-          width: 600,
-          child: child
-      ),
+        Navigator.canPop(context) ? BackButton(color: Theme.of(context).colorScheme.primary) : const SizedBox(),
+      ],
     );
   }
 }
